8ae018eb1098aceed517d81f8c317c7d07095917
@gadir.m <gadir.m@bestcomp.net>
2025-11-25 12:22:58 +0400

    scan 4

--- src/app/pages/fast-equipment-rental/fast-equipment-rental.component.ts
+++ src/app/pages/fast-equipment-rental/fast-equipment-rental.component.ts
@@ -1,17 +1,18 @@
 import { NgxScannerQrcodeComponent } from 'ngx-scanner-qrcode';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { fadeInUp } from 'src/app/shared/animations/fade-in-up.animation';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-fast-equipment-rental',
-  imports: [NgxScannerQrcodeComponent],
+  imports: [NgxScannerQrcodeComponent, JsonPipe],
   templateUrl: './fast-equipment-rental.component.html',
   styleUrl: './fast-equipment-rental.component.scss',
-  animations: [fadeInUp]
+  animations: [fadeInUp],
 })
 export class FastEquipmentRentalComponent implements OnInit, AfterViewInit {
-  @ViewChild('action', { static: false }) action!: NgxScannerQrcodeComponent;
+  @ViewChild('scanner', { static: false }) scanner!: NgxScannerQrcodeComponent;
 
   shotId: number | null = null;
   itemId: number | null = null;
@@ -22,22 +23,30 @@ export class FastEquipmentRentalComponent implements OnInit, AfterViewInit {
   currentX: number = 0;
   swipeThreshold = 50;
   swipedItem: any = null;
+  devices=signal<any>([]);
 
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.shotId = Number(params['shotId']);
       this.itemId = Number(params['itemId']);
     });
   }
 
   ngAfterViewInit(): void {
-    this.action.start();
+    this.scanner.start();
+    setTimeout(() => {
+      this.scanner.isReady.subscribe((ready) => {
+        if (ready) {
+          this.onScannerReady();
+        }
+      });
+    }, 0);
   }
 
   onScanForRental(results: any): void {
-    this.action.pause();
+    this.scanner.pause();
     const currentScanTime = results[0]?.time;
     if (currentScanTime - this.lastScanTime > 2000) {
       this.lastScanTime = currentScanTime;
@@ -45,12 +54,12 @@ export class FastEquipmentRentalComponent implements OnInit, AfterViewInit {
       console.log('Scanned:', this.result, this.lastScanTime);
     }
     setTimeout(() => {
-      this.action.play();
+      this.scanner.play();
     }, 2000);
   }
 
   onScanForRefund(results: any): void {
-    this.action.pause();
+    this.scanner.pause();
     const currentScanTime = results[0]?.time;
     if (currentScanTime - this.lastScanTime > 2000) {
       this.lastScanTime = currentScanTime;
@@ -58,18 +67,17 @@ export class FastEquipmentRentalComponent implements OnInit, AfterViewInit {
       console.log('Scanned:', this.result, this.lastScanTime);
     }
     setTimeout(() => {
-      this.action.play();
+      this.scanner.play();
     }, 2000);
   }
 
   equipmentConfirm() {
-    this.router.navigate(
-      ['/dashboard/equipment-confirmation'],
-      { queryParams: { status: 'rented' } }
-    );
+    this.router.navigate(['/dashboard/equipment-confirmation'], {
+      queryParams: { status: 'rented' },
+    });
   }
 
-  //#region Swipe Actions for Card Items
+  //#region Swipe scanners for Card Items
   startSwipe(event: TouchEvent) {
     this.startX = event.touches[0].clientX;
     // this.swipedItem = item;
@@ -80,7 +88,9 @@ export class FastEquipmentRentalComponent implements OnInit, AfterViewInit {
 
     this.currentX = event.touches[0].clientX - this.startX;
 
-    const card = (event.target as HTMLElement).closest('.card-item') as HTMLElement;
+    const card = (event.target as HTMLElement).closest(
+      '.card-item'
+    ) as HTMLElement;
     if (!card) return;
 
     // only allow left swipe
@@ -90,11 +100,13 @@ export class FastEquipmentRentalComponent implements OnInit, AfterViewInit {
   }
 
   endSwipe(event: TouchEvent) {
-    const card = (event.target as HTMLElement).closest('.card-item') as HTMLElement;
+    const card = (event.target as HTMLElement).closest(
+      '.card-item'
+    ) as HTMLElement;
     if (!card) return;
 
     if (this.currentX < -this.swipeThreshold) {
-      // reveal action button
+      // reveal scanner button
       card.style.transform = 'translateX(-130px)';
     } else {
       // reset position
@@ -108,8 +120,43 @@ export class FastEquipmentRentalComponent implements OnInit, AfterViewInit {
   }
 
   onDelete() {
-    console.log('Delete action triggered');
+    console.log('Delete scanner triggered');
   }
   //#endregion
-  
+
+  onScannerReady() {
+    const sub = this.scanner.devices.subscribe((devices) => {
+      if (!devices?.length) return;
+
+      this.devices.set(devices);
+
+      // try to find a rear/back/environment camera by label
+      const rear = devices.find((d) =>
+        /back|rear|trás|traseira|environment|ambiente/i.test(d.label)
+      );
+
+      // fall back to the last camera (often the rear on Android)
+      const chosen = rear ?? devices[devices.length - 1];
+
+      if (chosen) this.scanner.playDevice(chosen.deviceId);
+      sub.unsubscribe(); // pick once
+    });
+  }
+
+  //   onScannerInit(scanner: any) {
+  //   scanner.getMediaDevices().then((devices: any) => {
+  //              if (!devices?.length) return;
+
+  //       // try to find a rear/back/environment camera by label
+  //       const rear = devices.find((d: any) =>
+  //         /back|rear|trás|traseira|environment|ambiente/i.test(d.label)
+  //       );
+
+  //       // fall back to the last camera (often the rear on Android)
+  //       const chosen = rear ?? devices[devices.length - 1];
+
+  //       if (chosen) this.scanner.playDevice(chosen.deviceId);
+  //     console.log('Devices:', devices);
+  //   });
+  // }
 }
