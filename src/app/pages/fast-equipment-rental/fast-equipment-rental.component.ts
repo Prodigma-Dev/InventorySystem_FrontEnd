40629bc6d77bdaaded90528014ca366b6fab7405
@gadir.m <gadir.m@bestcomp.net>
2025-11-25 11:20:28 +0400

    scan 1

--- src/app/pages/fast-equipment-rental/fast-equipment-rental.component.ts
+++ src/app/pages/fast-equipment-rental/fast-equipment-rental.component.ts
@@ -111,22 +111,5 @@ export class FastEquipmentRentalComponent implements OnInit, AfterViewInit {
     console.log('Delete action triggered');
   }
   //#endregion
-
-  onScannerReady() {
-    const sub = this.action.devices.subscribe(devices => {
-      if (!devices?.length) return;
-
-      // try to find a rear/back/environment camera by label
-      const rear = devices.find(d =>
-        /back|rear|trás|traseira|environment|ambiente/i.test(d.label)
-      );
-
-      // fall back to the last camera (often the rear on Android)
-      const chosen = rear ?? devices[devices.length - 1];
-
-      if (chosen) this.action.playDevice(chosen.deviceId);
-      sub.unsubscribe(); // pick once
-    });
-  }
   
 }
